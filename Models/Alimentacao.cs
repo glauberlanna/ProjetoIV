25bf4e1c85dc124e4315cc19dd2bd19ef659c678
Glauber Lanna <glauberlanna@hotmail.com>
2025-03-21 11:49:46 -0300

    Add project files.

---
+++ Models/Alimentacao.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_jcm_g3_eixo_4_2025_1.Models
+{
+    [Table("Alimentações")]
+    public class Alimentacao
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Hora")]
+        public DateTime Hora { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Obrigatório Informar o Tipo de Alimentação!")]
+        [Display(Name = "Tipo de Alimentação")]
+        public TipoAlimentacao Tipo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a Quantidade de Alimento administrada!")]
+        [Display(Name = "Quantidade")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a Unidade de Medida!")]
+        [Display(Name = "Unidade de Medida")]
+        public UnidadeMedida Medida { get; set; }
+
+        [Required]
+        public int CachorroId { get; set; }
+
+        public Cachorro Cachorro { get; set; }
+
+
+    }
+
+    public enum TipoAlimentacao
+    {
+        Ração_Seca,
+        Ração_Úmida,
+        Alimento_Caseiro,
+        Petisco,
+        Alimento_Cru
+    }
+
+    public enum UnidadeMedida
+    {
+        Copo,
+        gramas,
+        Scoop,
+        Vasilhame
+    }
+}
---
+++ Models/Cachorro.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_jcm_g3_eixo_4_2025_1.Models
+{
+    [Table("Cachorros")]
+    public class Cachorro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome do cachorro!")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data de nascimento do cachorro!")]
+        [Display(Name = "Nascimento")]
+        public int Nascimento { get; set; }
+
+        [Display(Name = "Raça")]
+        public string Raca { get; set; }
+
+        public ICollection<Alimentacao> Alimentacoes { get; set; }
+
+    }
+}
