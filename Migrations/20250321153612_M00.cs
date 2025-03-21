41a51d91c40e8b91c36b35e1f578dca720f695be
Glauber Lanna <glauberlanna@hotmail.com>
2025-03-21 12:57:53 -0300

    Configuração do EntityFrameWork

---
+++ Migrations/20250321153612_M00.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_jcm_g3_eixo_4_2025_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class M00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cachorros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nascimento = table.Column<int>(type: "int", nullable: false),
+                    Raca = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cachorros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alimentações",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Hora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    Medida = table.Column<int>(type: "int", nullable: false),
+                    CachorroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alimentações", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alimentações_Cachorros_CachorroId",
+                        column: x => x.CachorroId,
+                        principalTable: "Cachorros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alimentações_CachorroId",
+                table: "Alimentações",
+                column: "CachorroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alimentações");
+
+            migrationBuilder.DropTable(
+                name: "Cachorros");
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projeto_jcm_g3_eixo_4_2025_1.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {       
+        }
+
+        public DbSet<Cachorro> Cachorros { get; set; }
+        public DbSet<Alimentacao> Alimentacoes { get; set; }
+    }
+}
